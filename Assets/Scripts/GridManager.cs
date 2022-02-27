5c67d259c09441ea1475d3e279a998562531ad12
_Justrishi <rishimud@gmail.com>
2022-02-27 17:18:20 +0530

    Trying gridLayout

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -7,7 +7,6 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int _width, _height;
     [SerializeField] private Tile _tilePrefab;
     [SerializeField] private Transform _cam;
-    [SerializeField] private GridLayoutGroup _layoutGroup;
     private Dictionary<Vector2, Tile> _tiles;
 
     void Start()
@@ -24,14 +23,13 @@ public class GridManager : MonoBehaviour
             {
                 var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
-                //var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
-                //spawnedTile.Init(isOffset);
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
                 _tiles[new Vector2(x, y)] = spawnedTile;
-                spawnedTile.transform.SetParent(_layoutGroup.transform, false);
             }
         }
 
-        //_cam.transform.position = new Vector3((float)_width / 2, (float)_height / 2 , -10);
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
     }
 
     public Tile GetTileAtPosition(Vector2 pos)  
