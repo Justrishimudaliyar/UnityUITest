e7b9b692da167e82a457aa6ed92b5ccc76e800fe
_Justrishi <rishimud@gmail.com>
2022-02-28 20:21:18 +0530

    AOI Logic implemented

--- Assets/Scripts/GridElement.cs
+++ Assets/Scripts/GridElement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 
 public class GridElement : MonoBehaviour
@@ -14,7 +15,16 @@ public class GridElement : MonoBehaviour
     public TextMeshProUGUI gridElementText;
     [SerializeField]
     private Button gridElementButton;
+    public bool isActive;
 
+    public event Action<int, int> OnClickGridElement;
+
+    private void Start()
+    {
+        isActive = false;
+        gridElementButton = gameObject.GetComponent<Button>();
+        gridElementButton.onClick.AddListener(OpenGridElement);
+    }
     public void SetPosition(int row, int col)
     {
         rowNumber = row;
@@ -24,17 +34,25 @@ public class GridElement : MonoBehaviour
     public void GenerateGridElementNumber(int columns)
     {
         gridElementNumber = rowNumber * columns + columnNumber + 1;
-        gridElementNumber = Random.Range(0, gridElementNumber);
+        gridElementNumber = UnityEngine.Random.Range(0, gridElementNumber);
     }
 
     public void GenerateGridElementColor()
     {
-        gridElementColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        gridElementColor = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
     }
 
+    public void OnGridElementClick()
+    {
+        OnClickGridElement?.Invoke(rowNumber, columnNumber);
+    }
     public void OpenGridElement()
     {
         gridElementText.text = gridElementNumber.ToString();
         gameObject.GetComponent<Image>().color = gridElementColor;
+        Debug.Log(this.gameObject.name);
+        isActive = true;
     }
+
+    
 }
--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 public class GridManager : MonoBehaviour
 {
     [SerializeField] private int _rows, _columns;
-    [SerializeField] private GameObject Column;
-    [SerializeField] private GameObject GridElement;
+    [SerializeField] private GameObject Row;
+    [SerializeField] private GridElement GridElement;
     [HideInInspector] public GridElement[ , ] Grid;
+    public int areaOfInterest;
 
     private void Start()
     {
@@ -19,20 +20,38 @@ public class GridManager : MonoBehaviour
     {
         for (int row = 0; row < _rows; row++)
         {
-            var newColumn = Instantiate(Column, transform.position, Quaternion.identity);
-            newColumn.transform.SetParent(gameObject.transform);
+            var newRow = Instantiate(Row, transform.position, Quaternion.identity);
+            newRow.transform.SetParent(gameObject.transform);
 
             for (int col = 0; col < _columns; col++)
             {
                 var newGridElement = Instantiate(GridElement, transform.position, Quaternion.identity);
-                newGridElement.transform.SetParent(newColumn.transform);
+                newGridElement.transform.SetParent(newRow.transform);
+                newGridElement.name = $"gridElement {row} {col}";
 
-                newGridElement.GetComponent<GridElement>().SetPosition(row, col);
-                newGridElement.GetComponent<GridElement>().GenerateGridElementNumber(_columns);
-                newGridElement.GetComponent<GridElement>().GenerateGridElementColor();
+                newGridElement.SetPosition(row, col);
+                newGridElement.GenerateGridElementNumber(_columns);
+                newGridElement.GenerateGridElementColor();
+                newGridElement.OnClickGridElement += OpenGridElements;
                 //newGridElement.GetComponent<GridElement>().OpenGridElement();
 
                 Grid[row, col] = newGridElement.GetComponent<GridElement>();
+            }
+        }
+    }
+
+    public void OpenGridElements(int row, int col)
+    {
+        for (int a = -areaOfInterest; a <= areaOfInterest; a++)
+        {
+            for (int b = -areaOfInterest; b <= areaOfInterest; b++)
+            {
+                if (row + a < 0 || col + b < 0 || row + a > _rows - 1 || col + b > _columns - 1 || Grid[row + a, col + b].isActive)
+                {
+                    continue;
+                }
+                Grid[row + a, col + b].OpenGridElement();
+                Grid[row + a, col + b].OnClickGridElement -= OpenGridElements;
             }
         }
     }
